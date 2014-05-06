d763a49f9decc7c3c4751835a0c0a3c339d6506b
redskycry <noreply>
2014-05-06 09:39:23 +0000

    Add component userdetail

---
+++ ui/userdetail.reel/userdetail.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/userdetail.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Userdetail
+ * @extends Component
+ */
+exports.Userdetail = Component.specialize(/** @lends Userdetail# */ {
+    constructor: {
+        value: function Userdetail() {
+            this.super();
+        }
+    }
+});
